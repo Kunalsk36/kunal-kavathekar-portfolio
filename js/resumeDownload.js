a887d675b1c6c64945fc0d3b115613816a1920a6
Kunal Shrikant Kavathekar <114307131+Kunalsk36@users.noreply.github.com>
2025-12-24 19:10:55 +0530

    Update resume link

--- js/resumeDownload.js
+++ js/resumeDownload.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (downloadCvBtn) {
           downloadCvBtn.addEventListener("click", function (e) {
-            var fileId = "1H6ertVxG8dX11VpsEBu4s77U_lqg2AXY";
+            var fileId = "14DanWBmL_Ee4Vrq8j4MiSBk_4re0T2BY";
             var downloadUrl =
               "https://drive.google.com/uc?export=download&id=" + fileId;
             var a = document.createElement("a");
@@ -15,4 +15,5 @@ document.addEventListener("DOMContentLoaded", function () {
             document.body.removeChild(a);
           });
         }
+
       });
